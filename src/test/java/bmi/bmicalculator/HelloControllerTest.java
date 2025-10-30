af0ce4b074e36ac0c4866915fdbb1f7cd5964566
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-10-30 19:50:11 +0200

    Update tests

--- src/test/java/bmi/bmicalculator/HelloControllerTest.java
+++ src/test/java/bmi/bmicalculator/HelloControllerTest.java
@@ -2,29 +2,40 @@ package bmi.bmicalculator;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloControllerTest {
 
     @Test
     void onENClick() {
+        ResourceBundle rb = ResourceBundle.getBundle("MessagesBundle", new Locale("en", "US"));
+        assertEquals("Calculate BMI", rb.getString("btnCalculate.text"));
     }
 
     @Test
     void onFRClick() {
+        ResourceBundle rb = ResourceBundle.getBundle("MessagesBundle", new Locale("fr", "FR"));
+        assertEquals("Hauteur (m)", rb.getString("lblHeight.text"));
     }
 
     @Test
     void onURClick() {
+        ResourceBundle rb = ResourceBundle.getBundle("MessagesBundle", new Locale("ur", "PA"));
+        assertEquals("وزن (کلوگرام)", rb.getString("lblWeight.text"));
     }
 
     @Test
     void onVIClick() {
+        ResourceBundle rb = ResourceBundle.getBundle("MessagesBundle", new Locale("vi", "VI"));
+        assertEquals("Đầu vào không hợp lệ, vui lòng nhập số hợp lệ.", rb.getString("lblInvalid.text"));
     }
 
     @Test
     void calculateBMI() {
         HelloController hc = new HelloController();
-        assertEquals(24.221453287197235, hc.calculateBMI(70, 170), 0.0001);
+        assertEquals(24.22, hc.calculateBMI(70, 170), 0.0001);
     }
 }
